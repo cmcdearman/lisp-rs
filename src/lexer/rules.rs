5e607a88150e911444666099e09e0cc7e21c9044
mathlet <mcdearmanchris@gmail.com>
2022-05-11 07:54:19 -0500

    first basic fully working lexer

--- src/lexer/rules.rs
+++ src/lexer/rules.rs
@@ -1,5 +1,8 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use crate::T;
 use crate::token::TokenKind;
+use crate::token::TokenKind::Number;
 
 pub(crate) const fn unambiguous_single_char(c: char) -> Option<TokenKind> {
     Some(match c {
@@ -29,8 +32,39 @@ fn match_keyword(input: &str, keyword: &str) -> Option<u32> {
         .then(|| keyword.len() as u32)
 }
 
+fn match_regex(input: &str, r: &Regex) -> Option<u32> {
+    r.find(input).map(|regex_match| regex_match.end() as u32)
+}
+
+lazy_static! {
+    static ref STRING_REGEX: Regex =
+        Regex::new(r#"^"((\\"|\\\\)|[^\\"])*""#).unwrap();
+    static ref COMMENT_REGEX: Regex =
+        Regex::new(r#"^//[^\n]*\n"#).unwrap();
+    static ref NUMBER_REGEX: Regex =
+        Regex::new(r#"^((\d+(\.\d+)?)|(\.\d+))([Ee](\+|-)?\d+)?"#).unwrap();
+    static ref IDENTIFIER_REGEX: Regex =
+        Regex::new(r##"^([A-Za-z]|_)([A-Za-z]|_|\d)*"##).unwrap();
+}
+
 pub(crate) fn get_rules() -> Vec<Rule> {
     vec![
+        Rule {
+            kind:    T![comment],
+            matches: move |input| match_regex(input, &COMMENT_REGEX),
+        },
+        Rule {
+            kind:    T![ident],
+            matches: |input| match_regex(input, &IDENTIFIER_REGEX),
+        },
+        Rule {
+            kind:    T![number],
+            matches: |input| match_regex(input, &NUMBER_REGEX),
+        },
+        Rule {
+            kind:    T![string],
+            matches: move |input| match_regex(input, &STRING_REGEX),
+        },
         Rule {
             kind: T![let],
             matches: |input| match_keyword(input, "let")
