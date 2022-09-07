9a57b3525bd697e5d7afdb157aaa02abd98bd668
mathlet <mcdearmanchris@gmail.com>
2022-09-07 05:39:01 -0500

    pass ast as ref in eval

--- src/eval.rs
+++ src/eval.rs
@@ -1,11 +1,11 @@
 use crate::{ast::{Sexpr, Atom, Lit}, env::Env};
 
-pub fn eval(ast: Vec<Sexpr>, pos: usize, env: Env) -> Result<Sexpr, String> {
+pub fn eval(ast: &Vec<Sexpr>, pos: usize, env: Env) -> Result<Sexpr, String> {
     match &ast[pos] {
         lit @ Sexpr::Atom(Atom::Lit(_)) => Ok(lit.clone()),
         Sexpr::Atom(Atom::Sym(name)) => Ok(env.find(name.clone())?.clone()),
         Sexpr::Cons(d, n) => todo!(),
-        Sexpr::Fn(_) => todo!(),
+        Sexpr::Fn(f) => todo!(),
         Sexpr::Nil => todo!(),
     }
 }
