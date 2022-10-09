bb3832005f8836fe5acbb17bac3c536dba5c7144
cmcdearman <mcdearmanchris@gmail.com>
2022-10-09 10:23:18 -0500

    change name to lust

--- src/eval.rs
+++ src/eval.rs
@@ -1,14 +1,16 @@
-use crate::{ast::{Sexpr, Atom, Lit}, env::Env};
+use crate::{
+    ast::{Atom, Lit, Sexpr},
+    env::Env,
+};
 
 pub fn eval(ast: &Vec<Sexpr>, pos: usize, env: Env) -> Result<Sexpr, String> {
     match &ast[pos] {
         lit @ Sexpr::Atom(Atom::Lit(_)) => Ok(lit.clone()),
         Sexpr::Atom(Atom::Sym(name)) => Ok(env.find(name.clone())?.clone()),
         Sexpr::Cons(d, n) => {
-
             todo!()
         }
-        Sexpr::Fn(f) => todo!(),
+        Sexpr::Fn(_) => Err("unexpected form".to_string()),
         Sexpr::Nil => todo!(),
     }
 }
