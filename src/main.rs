1af90c97607e092ee6b6f8b400ffec12dd77f4f6
mathlet <mcdearmanchris@gmail.com>
2022-10-19 18:21:34 -0500

    first repl

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ mod env;
 mod eval;
 mod lex;
 mod parse;
+mod repl;
 mod token;
 
+use crate::eval::eval;
 use crate::lex::lex;
 use crate::parse::parse;
 use crate::token::TokenStream;
-use crate::eval::eval;
 use std::{env as fs_env, fs};
+use crate::repl::repl;
 
 fn main() {
     // let dir = fs_env::current_dir().unwrap();
@@ -19,15 +21,16 @@ fn main() {
     // )).expect("Something went wrong reading the file");
 
     // let input = "(+ (/ 6 (* 1.5 2)) (- 1 (mod 5 2))) ; 2.0";
-    let input = "(* (+ 2 3) 4)";
-    let mut tokens = lex(input);
-    // for t in &tokens {
-    //     println!("{:?}", t);
-    // }
+    // // let input = "(* (+ 2 3) 4)";
+    // let mut tokens = lex(input);
+    // // for t in &tokens {
+    // //     println!("{:?}", t);
+    // // }
 
-    let ast = parse(&mut TokenStream::new(tokens).peekable());
+    // let ast = parse(&mut TokenStream::new(tokens).peekable());
 
-    println!("{}", ast);
-    let val = eval(&ast, &mut env::default_env());
-    println!("{}", eval(&ast, &mut env::default_env()).expect("invalid ast"));
+    // println!("{}", ast);
+    // let val = eval(&ast, &mut env::default_env());
+    // println!("{}", eval(&ast, &mut env::default_env()).expect("invalid ast"));
+    repl(&mut env::default_env());
 }
