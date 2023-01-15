ffea260a8cd688f9b25de5269a421b333ae500f2
mcdearman <mcdearmanchris@gmail.com>
2023-01-15 02:50:14 -0600

    normalize rationals by default

--- src/ast/number/rational.rs
+++ src/ast/number/rational.rs
@@ -1,28 +1,29 @@
-use std::fmt::{Display, Write};
+use std::fmt::Display;
 
 use super::integer::Integer;
 
 #[derive(Debug, Clone)]
-pub struct Rational(Sign, Integer, Integer);
+pub struct Rational {
+    numerator: Integer,
+    denominator: Integer,
+}
+
+impl Rational {
+    pub fn new(numerator: Integer, denominator: Integer) -> Self {
+        Self {
+            numerator,
+            denominator,
+        }
+        .normalize()
+    }
+
+    fn normalize(&self) -> Self {
+        todo!()
+    }
+}
 
 impl Display for Rational {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}{}/{}", self.0, self.1, self.2)
+        write!(f, "{}/{}", self.numerator, self.denominator)
     }
 }
-
-#[derive(Debug, Clone, Copy)]
-pub enum Sign {
-    Plus,
-    Minus,
-}
-
-impl Display for Sign {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Sign::Plus => f.write_char('+'),
-            Sign::Minus => f.write_char('-'),
-        }
-    }
-}
-
