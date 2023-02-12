77271fe54bc9c40561fb5cf4eae9c2720900d59d
mcdearman <mcdearmanchris@gmail.com>
2023-02-12 14:58:48 -0600

    start vm

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,96 +1,65 @@
-// pub struct VM {
-//     registers: [i32; 32],
-//     pc: usize,
-//     program: Vec<u8>,
-// }
-
-// impl VM {
-//     pub fn new() -> Self {
-//         Self {
-//             registers: [0; 32],
-//             pc: 0,
-//             program: Vec::new(),
-//         }
-//     }
-
-//     pub fn run(&mut self) {
-//         loop {
-//             if self.pc >= self.program.len() {
-//                 break;
-//             }
-//             match self.decode_opcode() {
-//                 Opcode::Halt => {
-//                     println!("Halt encountered");
-//                     return;
-//                 },
-//             }
-//         }
-//     }
-
-//     fn decode_opcode(&mut self) -> Opcode {
-//         let opcode = Opcode::from(self.program[self.pc]);
-//         self.pc += 1;
-//         return opcode;
-//     }
-// }
-
-// #[derive(Debug, PartialEq)]
-// pub enum Opcode {
-//     // LoadConst(usize, i32),
-//     // Add(usize, usize, usize),
-//     // Jump(usize),
-//     // Return,
-//     Halt,
-// }
-
-// impl From<u8> for Opcode {
-//     fn from(v: u8) -> Self {
-//         match v {
-//             0 => return Opcode::Halt,
-//         }
-//     }
-// }
-
-// #[derive(Debug, PartialEq)]
-// pub struct Instruction {
-//     opcode: Opcode,
-// }
-
-// impl Instruction {
-//     pub fn new(opcode: Opcode) -> Instruction {
-//         Instruction { opcode: opcode }
-//     }
-// }
-
-use std::fmt::Display;
-
-#[derive(Debug)]
-pub enum Opcode {
-    Return,
+pub struct VM {
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>,
 }
 
-impl Display for Opcode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Opcode::Return => "Return",
-            }
-        )
-    }
-}
-
-pub struct Chunk {
-    code: Vec<u8>,
-}
-
-impl Chunk {
+impl VM {
     pub fn new() -> Self {
-        Self { code: Vec::new() }
+        Self {
+            registers: [0; 32],
+            pc: 0,
+            program: Vec::new(),
+        }
     }
 
-    pub fn disassemble(&self) -> Vec<Opcode> {
-        todo!()
+    pub fn run(&mut self) {
+        loop {
+            if self.pc >= self.program.len() {
+                break;
+            }
+            match self.decode_opcode() {
+                Opcode::Halt => {
+                    println!("Halt encountered");
+                    return;
+                },
+            }
+        }
+    }
+
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
     }
 }
+
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    // LoadConst(usize, i32),
+    // Add(usize, usize, usize),
+    // Jump(usize),
+    // Return,
+    Halt,
+}
+
+impl From<u8> for Opcode {
+    fn from(v: u8) -> Self {
+        match v {
+            0 => return Opcode::Halt,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode: opcode }
+    }
+}
+
+
