735b1b2ad21a78c3d2316b9565f21e2dd5a7c429
mcdearman <mcdearmanchris@gmail.com>
2023-02-13 04:24:58 -0600

    remove main comment

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,3 @@ use crate::{
 fn main() {
     repl();
 }
-
-// fn main() {
-//     let input = "(+ 3 4)";
-
-//     let tokens = TokenStream::new(
-//         lex::lex(&input)
-//             .into_iter()
-//             .filter(|t| t.kind != TokenKind::Comment)
-//             .collect(),
-//     );
-
-//     let ast = parse::parse(&mut tokens.clone().peekable());
-
-//     for t in tokens {
-//         println!("{:?} {}", t, t.lit)
-//     }
-//     println!("{}", ast);
-// }
