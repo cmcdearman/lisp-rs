3230a238ed32bf6dcea0a226243238f3dc4c2794
Chris McDearman <mcdearmanchris@gmail.com>
2023-02-19 16:07:29 -0600

    fix default_env sum list

--- src/interpreter/default_env.rs
+++ src/interpreter/default_env.rs
@@ -1,67 +1,67 @@
-// use crate::parser::sexpr::{env::Env, Atom, Lit, Number, Sexpr};
+use crate::parser::sexpr::{env::Env, Atom, Lit, Number, Sexpr};
 
-// use super::runtime_error::RuntimeError;
+use super::runtime_error::{Result, RuntimeError};
 
-// pub fn default_env() -> Env {
-//     let mut env = Env::new();
-//     env.define(
-//         String::from("+"),
-//         Sexpr::NativeFn(|_, args| Ok(sum_number_list(args)?)),
-//     );
-//     // env.define(
-//     //     String::from("-"),
-//     //     Sexpr::NativeFn(|_, args| Ok((sub_number_list(args)?))),
-//     // );
-//     // env.define(
-//     //     String::from("*"),
-//     //     Sexpr::NativeFn(|_, args| Ok((mul_number_list(args)?))),
-//     // );
-//     // env.define(
-//     //     String::from("/"),
-//     //     Sexpr::NativeFn(|_, args| Ok((quo_number_list(args)?))),
-//     // );
-//     // env.define(
-//     //     String::from("let"),
-//     //     Sexpr::NativeFn(|_, args| Ok((sum_number_list(args)?))),
-//     // );
-//     // env.define(
-//     //     String::from("mod"),
-//     //     Sexpr::NativeFn(|_, args| Ok((mod_number_list(args)?))),
-//     // );
-//     // env.define(
-//     //     String::from("fn"),
-//     //     Sexpr::NativeFn(|env, args| {
-//     //         if !(2..4).contains(&args.len()) {
-//     //             return Err("not enough arguments for function declaration".to_string());
-//     //         }
-//     //         let lambda_args = &args[0];
-//     //         let body = &args[1];
-//     //         let mut fn_args;
-//     //         if args.len() == 3 {
-//     //             fn_args = &args[2];
-//     //         }
-//     //         // Ok(Sexpr::Lambda(Lambda { env, args: lambda_args, body }))
-//     //         todo!()
-//     //     }),
-//     // );
-//     // env.define(
-//     //     String::from("type-of"),
-//     //     Sexpr::NativeFn(|env, args| todo!()),
-//     // );
-//     env
-// }
+pub fn default_env() -> Env {
+    let mut env = Env::new();
+    env.define(
+        String::from("+"),
+        Sexpr::NativeFn(|_, args| Ok(sum_number_list(args)?)),
+    );
+    // env.define(
+    //     String::from("-"),
+    //     Sexpr::NativeFn(|_, args| Ok((sub_number_list(args)?))),
+    // );
+    // env.define(
+    //     String::from("*"),
+    //     Sexpr::NativeFn(|_, args| Ok((mul_number_list(args)?))),
+    // );
+    // env.define(
+    //     String::from("/"),
+    //     Sexpr::NativeFn(|_, args| Ok((quo_number_list(args)?))),
+    // );
+    // env.define(
+    //     String::from("let"),
+    //     Sexpr::NativeFn(|_, args| Ok((sum_number_list(args)?))),
+    // );
+    // env.define(
+    //     String::from("mod"),
+    //     Sexpr::NativeFn(|_, args| Ok((mod_number_list(args)?))),
+    // );
+    // env.define(
+    //     String::from("fn"),
+    //     Sexpr::NativeFn(|env, args| {
+    //         if !(2..4).contains(&args.len()) {
+    //             return Err("not enough arguments for function declaration".to_string());
+    //         }
+    //         let lambda_args = &args[0];
+    //         let body = &args[1];
+    //         let mut fn_args;
+    //         if args.len() == 3 {
+    //             fn_args = &args[2];
+    //         }
+    //         // Ok(Sexpr::Lambda(Lambda { env, args: lambda_args, body }))
+    //         todo!()
+    //     }),
+    // );
+    // env.define(
+    //     String::from("type-of"),
+    //     Sexpr::NativeFn(|env, args| todo!()),
+    // );
+    env
+}
 
-// fn sum_number_list(args: Vec<Sexpr>) -> Result<Sexpr, RuntimeError> {
-//     Ok(Sexpr::Atom(Atom::Lit(Lit::Number(args.iter().fold(
-//         Number::Fixnum(0),
-//         |acc, s| -> Number {
-//             match s {
-//                 Sexpr::Atom(Atom::Lit(Lit::Number(n))) => Ok((acc + *n)?),
-//                 _ => Err(RuntimeError::IvalidFunctionArgumentsError),
-//             }
-//         },
-//     )))))
-// }
+fn sum_number_list(args: Vec<Sexpr>) -> Result<Sexpr> {
+    Ok(Sexpr::Atom(Atom::Lit(Lit::Number(args.iter().try_fold(
+        Number::Fixnum(0),
+        |acc, s| -> Result<Number> {
+            match s {
+                Sexpr::Atom(Atom::Lit(Lit::Number(n))) => acc + n.clone(),
+                _ => Err(RuntimeError::IvalidFunctionArgumentsError),
+            }
+        },
+    )?))))
+}
 
 // fn gcd(a: i64, b: i64) -> i64 {
 //     match b {
