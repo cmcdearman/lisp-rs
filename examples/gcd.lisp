35cd71930deeead459933cda67b93bcf41e5ca75
Chris McDearman <mcdearmanchris@gmail.com>
2023-02-26 11:59:16 -0600

    change gcd

--- examples/gcd.lisp
+++ examples/gcd.lisp
@@ -1,11 +1,12 @@
 (def gcd 
   (fn (a b) 
-    (if (eq b 0) 
-      (a) 
+    (if (= b 0) 
+      a 
       (gcd b (mod a b)))))
 
-(def gcd (fn (a b) (if (= b 0) a (gcd b (mod a b)))))
 
-(if (> 3 5) (+ 4 5) (- 3 6))
-
-(def gtr (fn (a b) (if (> a b) (+ 4 5) (- 3 6))))
+(def gcd 
+  (fn (a b) 
+    (match b
+      ()
+      (gcd b (mod a b)))))
