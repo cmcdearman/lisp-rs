2a756941d84c4d6cacec7b57c0561599a32b669c
Chris McDearman <mcdearmanchris@gmail.com>
2023-03-01 06:55:31 -0600

    add types module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 mod interpreter;
 mod parser;
 
-use std::fmt::format;
-
-use interpreter::default_env;
-use itertools::join;
-use parser::Parser;
-
 use crate::interpreter::repl::repl;
 
 fn main() {
