5d7cce909afdc51e4191fe8aaca14679d00443b5
Chris McDearman <mcdearmanchris@gmail.com>
2023-04-26 20:23:25 -0500

    refactor parsing

---
+++ src/lower.rs
@@ -0,0 +1,5 @@
+/*
+ * This file is responsible for lowering the AST to LIR. LIR is an ANF form of
+ * the AST. This is where the AST is transformed into a form that is easier to
+ * compile to machine code.
+ */
