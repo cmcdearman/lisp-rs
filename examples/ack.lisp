a3fa9e7bf07872a8bebcbac5622996c7f8903c74
mcdearman <mcdearmanchris@gmail.com>
2023-05-01 20:27:04 -0500

    update examples

--- examples/ack.lisp
+++ examples/ack.lisp
@@ -3,3 +3,12 @@
     (cond ((= m 0) (+ n 1))
           ((= n 0) (ack (- m 1) 1))
           (:else (ack (- m 1) (ack m (- n 1)))))))
+
+;; let ack m n = 
+;;   if m = 0 then n + 1 
+;;   elif n = 0 then ack (m - 1) 1 
+;;   else ack (m - 1) (ack m (n - 1))
+(let ack (m n)
+  (cond ((= m 0) (+ n 1))
+        ((= n 0) (ack (- m 1) 1))
+        (:else (ack (- m 1) (ack m (- n 1))))))
