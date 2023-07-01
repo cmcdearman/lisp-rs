de43337e833845fd87a7c020cad6a01684f2dd69
Chris McDearman <mcdearmanchris@gmail.com>
2023-07-01 17:16:34 -0500

    update ack

--- examples/ack.scm
+++ examples/ack.scm
@@ -1,4 +1,4 @@
 (define (ack m n)
-  (cond ((= m 0) (+ n 1))
-        ((= n 0) (ack (- m 1) 1))
+  (cond ((eq m 0) (+ n 1))
+        ((eq n 0) (ack (- m 1) 1))
         (else (ack (- m 1) (ack m (- n 1))))))
