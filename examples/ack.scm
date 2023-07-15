07ea0ae299df9d800697438c4250a10dc55d01a3
Chris McDearman <mcdearmanchris@gmail.com>
2023-07-15 15:10:28 -0500

    idk

--- examples/ack.scm
+++ examples/ack.scm
@@ -2,3 +2,26 @@
   (cond ((eq m 0) (+ n 1))
         ((eq n 0) (ack (- m 1) 1))
         (else (ack (- m 1) (ack m (- n 1))))))
+
+;; fn ack m n =
+;;  if m == 0 then n + 1
+;;  else if n == 0 then ack (m - 1) 1
+;;  else ack (m - 1) (ack m (n - 1))
+(fn ack (m n)
+  (cond ((eq m 0) (+ n 1))
+        ((eq n 0) (ack (- m 1) 1))
+        (else (ack (- m 1) (ack m (- n 1))))))
+
+(fn ack (m n)
+  (if ((= m 0) (+ n 1))
+  ((= n 0) (ack (- m 1) 1))
+  (ack (- m 1) (ack m (- n 1)))))
+
+;; let x = 1
+(let x 1)
+
+;; let x = 1 in x + 1
+(let x 1 (+ x 1))
+
+;; let x = 1 in let y = 2 in x + y
+(let [x y] [1 2] (+ x y))
