baaab411f9b4941fdd31dbeae4bc2c246d3cc3ac
Chris McDearman <mcdearmanchris@gmail.com>
2023-09-03 21:21:15 -0500

    update guide

--- examples/guide.scm
+++ examples/guide.scm
@@ -1,7 +1,7 @@
 ;; global variables
-(let a 10)
+(let x 10)
 ;; global constants
-(const a 10)
+(const x 10)
 
 ;; `let` can bind to lambdas
 (let gcd (lambda (a b)
@@ -21,16 +21,27 @@
     (0 a)
     (_ (gcd b (% a b)))))
 
-(let ((a 10) 
-      (b 5))
-  (+ a b))
+(let (fib n)
+  (match n
+    (0 0)
+    (1 1)
+    (_ (+ (fib (- n 1)) (fib (- n 2))))))
 
-(struct point (x y))
-
-(struct vec (arr len))
+;; or you can match to declare functions
+(let (fib 0) 0)
+(let (fib 1) 1)
+(let (fib n) (+ (fib (- n 1)) (fib (- n 2))))
 
 (let (fib n)
   (if ((<= n 1) n)
   (+ (fib (- n 1)) (fib (- n 2)))))
 
 (println (fib 100000))
+
+;; Macros
+(lets ((a 10) 
+      (b 5))
+  (+ a b))
+
+(struct point (x y))
+
