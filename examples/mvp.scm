7b0f2e1762b76f0f8ac85b214b915fd3d1768305
mcdearman <mcdearmanchris@gmail.com>
2024-01-21 18:50:07 -0600

    update

--- examples/mvp.scm
+++ examples/mvp.scm
@@ -104,7 +104,7 @@
 (let (ack m n)
   (cond ((= m 0) (+ n 1))
         ((= n 0) (ack (- m 1) 1))
-        (t (ack (- m 1) (ack m (- n 1))))))
+        ('t (ack (- m 1) (ack m (- n 1))))))
 
 ;; modules
 (module List
